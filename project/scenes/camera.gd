4e3632e5167a52383352a4c63c7567d459f0f870
benpm <schwalegos@gmail.com>
2024-07-28 13:26:47 -0600

    camera panning fix

--- project/scenes/camera.gd
+++ project/scenes/camera.gd
@@ -5,13 +5,12 @@ var cam_pos_clicked := Vector2()
 
 func _process(_delta: float):
 	# Pan the camera on right click
-	var mouse_pos = get_global_mouse_position()
-	var cam_pos = position
+	var mouse_pos = get_viewport().get_mouse_position() / zoom
 	if Input.is_action_just_pressed("alt_click") or Input.is_action_just_released("alt_click"):
 		mouse_pos_clicked = mouse_pos
-		cam_pos_clicked = cam_pos
+		cam_pos_clicked = offset
 	if Input.is_action_pressed("alt_click"):
-		position = cam_pos_clicked + (mouse_pos_clicked - mouse_pos)
+		offset = cam_pos_clicked + (mouse_pos_clicked - mouse_pos)
 
 	# Zoom the camera
 	if Input.is_action_just_pressed("zoom_in"):
